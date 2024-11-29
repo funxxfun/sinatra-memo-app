41c764f8279105cdbc8d224422f990fc43e3e53d
funxxfun <23shirasawa@gmail.com>
2024-11-30 00:28:41 +0900

    ファイルの名称を変更

---
+++ app.rb
@@ -0,0 +1,14 @@
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/memos' do
+  erb :index
+end
+get '/memos/new' do
+  erb :new
+end
+post '/memos' do
+  # メモを保存する処理をここに書く
+  redirect '/memos'
+end
